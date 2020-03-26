b1f977e5cdfa2bb2da9e87857fc630edcb4bdbdb
Glumsik <glumsik@icloud.com>
2020-03-27 01:05:53 +0300

    Add files via upload

---
+++ precache-manifest.e1404a8da8e3e4d70ed41d90b418c0d4.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6be91ca0a8fd7e23fda07d2d11f5400d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8dabaea8efded3d001d",
+    "url": "/static/css/main.c8695052.chunk.css"
+  },
+  {
+    "revision": "9f273765a42fa3d0b9e4",
+    "url": "/static/js/2.8dcc7bf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8dcc7bf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8dabaea8efded3d001d",
+    "url": "/static/js/main.a58cfbe5.chunk.js"
+  },
+  {
+    "revision": "415e9bcf6c081ea50565",
+    "url": "/static/js/runtime-main.f8c5b4be.js"
+  },
+  {
+    "revision": "651f02a2a765a5f313e851f185626b08",
+    "url": "/static/media/bg.651f02a2.png"
+  },
+  {
+    "revision": "6dc738ae4ba5364195d3cc72b6a0fa62",
+    "url": "/static/media/bg.6dc738ae.png"
+  },
+  {
+    "revision": "b6adce37ed8d0c281ca733d4db800d56",
+    "url": "/static/media/bg.b6adce37.png"
+  },
+  {
+    "revision": "ac85f32680059b0bcf1a6af8ae9c11cb",
+    "url": "/static/media/bg_3.ac85f326.png"
+  },
+  {
+    "revision": "30d1a15dc97f75d854ed2988c71ba74c",
+    "url": "/static/media/bg_SectionSecond.30d1a15d.png"
+  },
+  {
+    "revision": "d3883d314578bc3f41d56045c1e501fd",
+    "url": "/static/media/bg_section1.d3883d31.png"
+  },
+  {
+    "revision": "448c54628d54fa41255083054987aad1",
+    "url": "/static/media/ice_1.448c5462.png"
+  },
+  {
+    "revision": "44cdf642d7404cdb5a890af41d494d4a",
+    "url": "/static/media/ice_1.44cdf642.png"
+  },
+  {
+    "revision": "8e55e83c83c5b9d6a788b0c0ba8dc037",
+    "url": "/static/media/ice_2.8e55e83c.png"
+  },
+  {
+    "revision": "89f148e5fc407214af1784c8494aa3cf",
+    "url": "/static/media/ice_3.89f148e5.png"
+  },
+  {
+    "revision": "ce8f944eff21d0de156bac6d97cc7eaa",
+    "url": "/static/media/ice_3.ce8f944e.png"
+  },
+  {
+    "revision": "a310c685d5081591feca5e35e248f215",
+    "url": "/static/media/ice_4.a310c685.png"
+  }
+]);
